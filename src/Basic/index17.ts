9023f2119ba407b4b0cd1387976ee0ba0a975efe
jujihong <jujihong2@gmail.com>
2023-11-05 16:42:45 +0900

    feat: 타입스크립트 flat method

--- src/Basic/index17.ts
+++ src/Basic/index17.ts
@@ -63,3 +63,29 @@ const add4 = addBind.bind(null, 1, 2, 3);
 const add5 = addBind.bind(null, 1, 2, 3, 4);
 
 const add6 = addBind.bind(null, 1, 2, 3, 4, 5);
+
+const flatExam = [1, 2, 3, [1, 2], [[1], [2]]].flat();
+const flatExam2 = [1, 2, 3, [1, 2]].flat();
+
+type AAAA = { name: string; age: number };
+
+type NNNN = AAAA['1' extends number ? 'age' : 'name']; // string
+
+type SSSS = AAAA[1 extends number ? 'age' : 'name']; // number
+
+const flatExam3 = [1, 2, 3, [1, 2], [[1], [2]]].flat(2);
+
+// flat<A, D extends number = 1>(
+//         this: A,
+//         depth?: D
+//     ): FlatArray<A, D>[]
+
+//FlatArray<(number[] | number[][] | number[][][]), 2>[]
+
+//FlatArray<(number | number[] | number[][]), 1>[]
+
+//FlatArray<(number | number[]), 0>[]
+
+//FlatArray<(number), -1>[]
+
+//number[]
